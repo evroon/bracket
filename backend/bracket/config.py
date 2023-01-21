d9f7efa3dff2481d42ccabfd83cf0e29629e6cb1
Erik Vroon <erik.vroon22@gmail.com>
2023-01-21 07:21:25 -0800

    Add Sentry (#96)

--- backend/bracket/config.py
+++ backend/bracket/config.py
@@ -2,6 +2,7 @@ import logging
 import os
 from enum import auto
 
+import sentry_sdk
 from pydantic import BaseSettings, PostgresDsn
 
 from bracket.utils.types import EnumAutoStr
@@ -10,6 +11,7 @@ from bracket.utils.types import EnumAutoStr
 class Environment(EnumAutoStr):
     PRODUCTION = auto()
     DEVELOPMENT = auto()
+    DEMO = auto()
     CI = auto()
 
     def get_env_filepath(self) -> str:
@@ -17,6 +19,7 @@ class Environment(EnumAutoStr):
             Environment.PRODUCTION: 'prod.env',
             Environment.DEVELOPMENT: 'dev.env',
             Environment.CI: 'ci.env',
+            Environment.DEMO: 'demo.env',
         }[self]
 
     def get_log_level(self) -> int:
@@ -24,6 +27,7 @@ class Environment(EnumAutoStr):
             Environment.CI: logging.WARNING,
             Environment.DEVELOPMENT: logging.DEBUG,
             Environment.PRODUCTION: logging.INFO,
+            Environment.DEMO: logging.INFO,
         }[self]
 
 
@@ -34,7 +38,17 @@ class Config(BaseSettings):
     cors_origin_regex: str = ''
     admin_email: str | None = None
     admin_password: str | None = None
+    sentry_dsn: str | None = None
 
 
 environment = Environment(os.getenv('ENVIRONMENT', 'DEVELOPMENT'))
 config = Config(_env_file=environment.get_env_filepath())
+
+
+def init_sentry() -> None:
+    if config.sentry_dsn:
+        sentry_sdk.init(
+            dsn=config.sentry_dsn,
+            environment=str(environment.value),
+            with_locals=False,
+        )
--- backend/precommit.sh
+++ backend/precommit.sh
@@ -3,6 +3,6 @@ set -evo pipefail
 
 black .
 dmypy run -- --follow-imports=normal --junit-xml= .
-ENVIRONMENT=CI pytest --cov --cov-report=xml .
+SQLALCHEMY_SILENCE_UBER_WARNING=1 ENVIRONMENT=CI pytest --cov --cov-report=xml .
 pylint alembic bracket tests
 isort .
