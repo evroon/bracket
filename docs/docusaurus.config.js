d94865d5b13a9ee5f3d86c861721c66d2e3c438e
Erik Vroon <erik.vroon@channable.com>
2023-04-18 20:33:00 +0200

    fixup! fixup! Add docusaurus docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -91,7 +91,7 @@ const config = {
             items: [
               {
                 label: 'Docs',
-                to: '/docs',
+                to: '/docs/intro',
               },
             ],
           },
