23699f688e9c42927d17819b210ac82baa816207
Erik Vroon <erik.vroon22@gmail.com>
2023-04-29 15:03:45 +0200

    Use reloadable uvicorn worker (#189)

---
+++ backend/bracket/uvicorn.py
@@ -0,0 +1,31 @@
+import os
+import signal
+import threading
+import time
+from typing import Any, Dict, List
+
+from uvicorn.workers import UvicornWorker
+
+
+class ReloaderThread(threading.Thread):
+    def __init__(self, worker: UvicornWorker, sleep_interval: float = 1.0):
+        super().__init__()
+        self._worker = worker
+        self._interval = sleep_interval
+
+    def run(self) -> None:
+        while True:
+            if not self._worker.alive:
+                os.kill(os.getpid(), signal.SIGINT)
+            time.sleep(self._interval)
+
+
+class RestartableUvicornWorker(UvicornWorker):
+    def __init__(self, *args: List[Any], **kwargs: Dict[str, Any]):
+        super().__init__(*args, **kwargs)
+        self._reloader_thread = ReloaderThread(self)
+
+    def run(self) -> None:
+        if self.cfg.reload:
+            self._reloader_thread.start()
+        super().run()
--- backend/run.sh
+++ backend/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ENVIRONMENT=DEVELOPMENT gunicorn -k uvicorn.workers.UvicornWorker bracket.app:app --bind localhost:8400 --workers 2 --reload
+ENVIRONMENT=DEVELOPMENT gunicorn -k bracket.uvicorn.RestartableUvicornWorker bracket.app:app --bind localhost:8400 --workers 1 --reload
