dc514b7c4a2ef5149aec7db4b5cfe3be95996d18
Erik Vroon <erik.vroon@channable.com>
2023-05-19 11:57:08 +0200

    Add run.sh to run both backend and frontend

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function run_frontend() {
+  cd frontend && yarn run dev
+}
+
+function run_backend() {
+  cd backend && ENVIRONMENT=DEVELOPMENT pipenv run gunicorn \
+      -k bracket.uvicorn.RestartableUvicornWorker \
+      bracket.app:app \
+      --bind localhost:8400 \
+      --workers 1 \
+      --reload
+}
+
+(trap 'kill 0' SIGINT;
+  run_frontend &
+  run_backend
+)
