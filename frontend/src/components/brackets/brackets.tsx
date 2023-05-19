a2485359f95c9581506eb36b1b13a7018d54c9d9
Erik Vroon <erik.vroon@channable.com>
2023-05-19 12:14:25 +0200

    fixup! fixup! Select active stage by default

--- frontend/src/components/brackets/brackets.tsx
+++ frontend/src/components/brackets/brackets.tsx
@@ -8,6 +8,30 @@ import { TournamentMinimal } from '../../interfaces/tournament';
 import { responseIsValid } from '../utils/util';
 import Round from './round';
 
+function getRoundsGridCols(
+  stages_map: { [p: string]: any },
+  activeStageId: number,
+  tournamentData: TournamentMinimal,
+  swrStagesResponse: SWRResponse,
+  swrUpcomingMatchesResponse: SWRResponse | null,
+  readOnly: boolean
+) {
+  const rounds = stages_map[activeStageId].rounds
+    .sort((r1: any, r2: any) => (r1.name > r2.name ? 1 : 0))
+    .map((round: StageInterface) => (
+      <Grid.Col sm={6} lg={4} xl={3} key={round.id}>
+        <Round
+          tournamentData={tournamentData}
+          round={round}
+          swrRoundsResponse={swrStagesResponse}
+          swrUpcomingMatchesResponse={swrUpcomingMatchesResponse}
+          readOnly={readOnly}
+        />
+      </Grid.Col>
+    ));
+  return rounds;
+}
+
 export default function Brackets({
   tournamentData,
   swrStagesResponse,
@@ -46,20 +70,21 @@ export default function Brackets({
   const stages_map = Object.fromEntries(
     swrStagesResponse.data.data.map((x: StageInterface) => [x.id, x])
   );
-
-  const rounds = stages_map[activeStageId].rounds
-    .sort((r1: any, r2: any) => (r1.name > r2.name ? 1 : 0))
-    .map((round: StageInterface) => (
-      <Grid.Col sm={6} lg={4} xl={3} key={round.id}>
-        <Round
-          tournamentData={tournamentData}
-          round={round}
-          swrRoundsResponse={swrStagesResponse}
-          swrUpcomingMatchesResponse={swrUpcomingMatchesResponse}
-          readOnly={readOnly}
-        />
-      </Grid.Col>
-    ));
+  const rounds =
+    stages_map[activeStageId].rounds.length > 0 ? (
+      getRoundsGridCols(
+        stages_map,
+        activeStageId,
+        tournamentData,
+        swrStagesResponse,
+        swrUpcomingMatchesResponse,
+        readOnly
+      )
+    ) : (
+      <Alert icon={<IconAlertCircle size={16} />} title="No rounds" color="blue" radius="lg">
+        There are no rounds in this stage yet
+      </Alert>
+    );
 
   return (
     <div>
