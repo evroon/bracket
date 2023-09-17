c96e9635607fc9ad119effbcb96ef88d63de3bd7
Erik Vroon <erik.vroon@channable.com>
2023-09-17 14:30:12 +0200

    Fix responsiveness homepage

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -12,20 +12,24 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-          <div style={{width: "40%", float: 'left', clear: 'left', paddingRight: "1rem", paddingLeft: "1rem", color: "white"}}>
-              <h1 className="hero__title">{siteConfig.title}</h1>
-              <p className="hero__subtitle">{siteConfig.tagline}</p>
-              <div className={styles.buttons}>
-                  <Link
-                      className="button button--secondary button--lg"
-                      to="/docs/intro">
-                      Get Started
-                  </Link>
+          <div className={styles.heroRow}>
+              <div className={clsx(styles.heroColumn, styles.heroColumn4)}>
+                  <h1 className="hero__title">{siteConfig.title}</h1>
+                  <p className="hero__subtitle">{siteConfig.tagline}</p>
+                  <div className={styles.buttons}>
+                      <Link
+                          className="button button--secondary button--lg"
+                          to="/docs/intro">
+                          Get Started
+                      </Link>
+                  </div>
               </div>
-          </div>
-          <div style={{width: "60%", float: 'right', clear: 'right'}}>
-              <div className="text--center">
-                  <img alt="Preview of the web interface" src={require('@site/static/img/schedule_preview.png').default} width="100%"/>
+              <div className={clsx(styles.heroColumn, styles.heroColumn6)}>
+                  <div className="text--center">
+                      <img alt="Preview of the web interface"
+                           src={require('@site/static/img/schedule_preview.png').default}
+                           width="100%"/>
+                  </div>
               </div>
           </div>
       </div>
