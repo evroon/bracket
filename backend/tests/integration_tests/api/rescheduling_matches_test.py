0bc5966e84859ff11496a2995291ea29178f69b9
Erik Vroon <erik.vroon@channable.com>
2023-11-11 12:17:36 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! Add tests for scheduling logic

---
+++ backend/tests/integration_tests/api/rescheduling_matches_test.py
@@ -0,0 +1,120 @@
+from bracket.logic.scheduling.builder import build_matches_for_stage_item
+from bracket.models.db.stage_item import StageItemCreateBody
+from bracket.models.db.stage_item_inputs import (
+    StageItemInputCreateBodyFinal,
+    StageItemInputCreateBodyTentative,
+)
+from bracket.sql.shared import sql_delete_stage_item_with_foreign_keys
+from bracket.sql.stage_items import sql_create_stage_item
+from bracket.sql.stages import get_full_tournament_details
+from bracket.utils.dummy_records import (
+    DUMMY_STAGE2,
+    DUMMY_STAGE_ITEM1,
+    DUMMY_STAGE_ITEM3,
+    DUMMY_TEAM1, DUMMY_COURT1,
+)
+from bracket.utils.http import HTTPMethod
+from bracket.utils.types import assert_some
+from tests.integration_tests.api.shared import (
+    SUCCESS_RESPONSE,
+    send_tournament_request,
+)
+from tests.integration_tests.models import AuthContext
+from tests.integration_tests.sql import (
+    inserted_stage,
+    inserted_team, inserted_court,
+)
+
+
+async def test_schedule_all_matches(
+    startup_and_shutdown_uvicorn_server: None, auth_context: AuthContext
+) -> None:
+    async with (
+        inserted_court(
+            DUMMY_COURT1.copy(update={'tournament_id': auth_context.tournament.id})
+        ) as stage_inserted_1,
+        inserted_stage(
+            DUMMY_STAGE2.copy(update={'tournament_id': auth_context.tournament.id})
+        ) as stage_inserted_1,
+        inserted_team(
+            DUMMY_TEAM1.copy(update={'tournament_id': auth_context.tournament.id})
+        ) as team_inserted_1,
+        inserted_team(
+            DUMMY_TEAM1.copy(update={'tournament_id': auth_context.tournament.id})
+        ) as team_inserted_2,
+        inserted_team(
+            DUMMY_TEAM1.copy(update={'tournament_id': auth_context.tournament.id})
+        ) as team_inserted_3,
+        inserted_team(
+            DUMMY_TEAM1.copy(update={'tournament_id': auth_context.tournament.id})
+        ) as team_inserted_4,
+    ):
+        tournament_id = assert_some(auth_context.tournament.id)
+        stage_item_1 = await sql_create_stage_item(
+            tournament_id,
+            StageItemCreateBody(
+                stage_id=assert_some(stage_inserted_1.id),
+                name=DUMMY_STAGE_ITEM1.name,
+                team_count=DUMMY_STAGE_ITEM1.team_count,
+                type=DUMMY_STAGE_ITEM1.type,
+                inputs=[
+                    StageItemInputCreateBodyFinal(
+                        slot=1,
+                        team_id=assert_some(team_inserted_1.id),
+                    ),
+                    StageItemInputCreateBodyFinal(
+                        slot=2,
+                        team_id=assert_some(team_inserted_2.id),
+                    ),
+                    StageItemInputCreateBodyFinal(
+                        slot=3,
+                        team_id=assert_some(team_inserted_3.id),
+                    ),
+                    StageItemInputCreateBodyFinal(
+                        slot=4,
+                        team_id=assert_some(team_inserted_4.id),
+                    ),
+                ],
+            ),
+        )
+        stage_item_2 = await sql_create_stage_item(
+            tournament_id,
+            StageItemCreateBody(
+                stage_id=assert_some(stage_inserted_1.id),
+                name=DUMMY_STAGE_ITEM3.name,
+                team_count=2,
+                type=DUMMY_STAGE_ITEM3.type,
+                inputs=[
+                    StageItemInputCreateBodyTentative(
+                        slot=1,
+                        winner_from_stage_item_id=stage_item_1.id,
+                        winner_position=1,
+                    ),
+                    StageItemInputCreateBodyTentative(
+                        slot=2,
+                        winner_from_stage_item_id=stage_item_1.id,
+                        winner_position=2,
+                    ),
+                ],
+            ),
+        )
+        await build_matches_for_stage_item(stage_item_1, tournament_id)
+        await build_matches_for_stage_item(stage_item_2, tournament_id)
+        stages = await get_full_tournament_details(tournament_id)
+
+        await sql_delete_stage_item_with_foreign_keys(stage_item_2.id)
+        await sql_delete_stage_item_with_foreign_keys(stage_item_1.id)
+
+    assert (
+        await send_tournament_request(
+            HTTPMethod.POST,
+            'schedule_matches',
+            auth_context,
+        )
+        == SUCCESS_RESPONSE
+    )
+
+    stage_item = stages[0].stage_items[0]
+    assert len(stage_item.rounds) == 3
+    for round_ in stage_item.rounds:
+        assert len(round_.matches) == 2
