5d549648b27ba33312351562ee41572b9aaa6ca9
Erik Vroon <erik.vroon22@gmail.com>
2024-01-17 11:54:02 +0100

    Fix translations on Vercel (#409)
    Translations are not shown properly in vercel, fix:
    https://github.com/i18next/next-i18next/issues/1552#issuecomment-981156476

--- frontend/next-i18next.config.js
+++ frontend/next-i18next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next-i18next').UserConfig} */
+const path = require('path');
 module.exports = {
   i18n: {
     locales: ['en', 'zh-CN'],
     defaultLocale: 'en',
+    localePath: path.resolve('./public/locales')
   },
 };
