6c36ef6800a313db44b629b8839fe16395d28d8a
Erik Vroon <erik.vroon@channable.com>
2024-02-06 21:45:01 +0100

    fixup! Dutch translations

--- frontend/next-i18next.config.js
+++ frontend/next-i18next.config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 module.exports = {
   i18n: {
-    locales: ['en', 'zh-CN'],
+    locales: ['en', 'nl', 'zh-CN'],
     defaultLocale: 'en',
     localePath: path.resolve('./public/locales')
   },
