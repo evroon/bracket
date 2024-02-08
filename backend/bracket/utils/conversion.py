06321ec33223194dc290e47ed2bf18c8bdc566d8
Erik Vroon <erik.vroon@channable.com>
2024-02-08 21:35:10 +0100

    fixup! fixup! fixup! fixup! fixup! Pydantic v2 migration

--- backend/bracket/utils/conversion.py
+++ backend/bracket/utils/conversion.py
@@ -1,5 +1,4 @@
 from collections.abc import Mapping
-from datetime import datetime
 from typing import Any
 
 from heliclockter import datetime_tz
@@ -22,16 +21,3 @@ def to_string_mapping(obj: BaseModel) -> Mapping[str, Any]:
     Turns a pydantic object into a string mapping to be used as database query
     """
     return {key: _map_to_str(value) for key, value in obj.model_dump(exclude_none=True).items()}
-
-
-def _handle_datetime(value: Any) -> Any:
-    if isinstance(value, datetime_tz):
-        return datetime.fromisoformat(value.isoformat())
-
-    return value
-
-
-def to_database_mapping(obj: BaseModel) -> Mapping[str, Any]:
-    return {
-        key: _handle_datetime(value) for key, value in obj.model_dump(exclude_none=True).items()
-    }
