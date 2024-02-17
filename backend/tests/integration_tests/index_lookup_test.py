ebee89822b38a89982e150a7c4e8f0536087d5c7
Erik Vroon <erik.vroon@channable.com>
2024-02-17 13:59:07 +0100

    Handle foreign key constraint errors

--- backend/tests/integration_tests/index_lookup_test.py
+++ backend/tests/integration_tests/index_lookup_test.py
@@ -1,5 +1,6 @@
 from bracket.database import database
 from bracket.utils.errors import (
+    foreign_key_violation_error_lookup,
     unique_index_violation_error_lookup,
 )
 
@@ -23,3 +24,20 @@ async def test_all_unique_indices_in_lookup() -> None:
 
     expected_indices = {ix.name for ix in unique_index_violation_error_lookup.keys()}
     assert indices == expected_indices
+
+
+async def test_known_foreign_keys_in_lookup() -> None:
+    query = """
+        SELECT conrelid::regclass AS table_name,
+               conname AS foreign_key
+        FROM   pg_constraint
+        WHERE  contype = 'f'
+        AND    connamespace = 'public'::regnamespace
+        ORDER  BY conrelid::regclass::text, contype DESC;
+    """
+    result = await database.fetch_all(query)
+    indices = {ix.foreign_key for ix in result}  # type: ignore[attr-defined]
+
+    for foreign_key in foreign_key_violation_error_lookup.keys():
+        msg = f"Unexpected foreign key in lookup: {foreign_key.value}"
+        assert foreign_key.name in indices, msg
