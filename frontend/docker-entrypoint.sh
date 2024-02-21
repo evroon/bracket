6879a9c7ad2ed4b86e1b7bf423149c244377e550
robigan <35210888+robigan@users.noreply.github.com>
2024-02-21 10:17:12 +0100

    Proposed changes to use variable lists to sub variables

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,14 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 set -evo pipefail
 
 # Script to replace `NEXT_PUBLIC_*` environment variables because they're set at build-time but we want to set them at runtime in `docker-compose.yml`
-echo "Check that we have NEXT_PUBLIC_API_BASE_URL, NEXT_PUBLIC_HCAPTCHA_SITE_KEY vars"
-test -n "$NEXT_PUBLIC_API_BASE_URL"
-test -n "$NEXT_PUBLIC_HCAPTCHA_SITE_KEY"
+# Define an array of environment variable names
+env_vars=("NEXT_PUBLIC_API_BASE_URL" "NEXT_PUBLIC_HCAPTCHA_SITE_KEY")
 
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_PLACEHOLDER_NEXT_PUBLIC_API_BASE_URL#$NEXT_PUBLIC_API_BASE_URL#g" # Replace NEXT_PUBLIC_API_BASE_URL
+echo "Checking that we have variables..."
+# Iterate over the array and perform checks
+for var in "${env_vars[@]}"; do
+  test -n "${!var}"
+done
 
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_PLACEHOLDER_NEXT_PUBLIC_HCAPTCHA_SITE_KEY#$NEXT_PUBLIC_HCAPTCHA_SITE_KEY#g" # Replace NEXT_PUBLIC_HCAPTCHA_SITE_KEY
+# Iterate over the array and perform substitutions
+for var in "${env_vars[@]}"; do
+  find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_PLACEHOLDER_$var#${!var}#g"
+done
 
 echo "Starting Nextjs"
 exec "$@"
