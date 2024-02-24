3aa7f61658cb628caf01cf878e8efd19dcbed880
Erik Vroon <erik.vroon@channable.com>
2024-02-24 12:25:20 +0100

    fixup! fixup! Stamp alembic head when recreating tables

--- backend/bracket/utils/alembic.py
+++ backend/bracket/utils/alembic.py
@@ -3,15 +3,15 @@ from alembic.config import Config
 from bracket.utils.logging import logger
 
 
-def get_alembic_config() -> None:
+def get_alembic_config() -> Config:
     return Config("alembic.ini")
 
 
 def alembic_run_migrations() -> None:
     logger.info("Running migrations")
-    command.upgrade(Config("alembic.ini"), "head")
+    command.upgrade(get_alembic_config(), "head")
 
 
 def alembic_stamp_head() -> None:
     logger.info("Overwriting current version to be the latest revision (head)")
-    command.stamp(Config("alembic.ini"), "head")
+    command.stamp(get_alembic_config(), "head")
