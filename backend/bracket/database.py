d77e74903cbfb3f8dcdc2a9db3633e2bde19570d
Danny Piper <djpiper28@gmail.com>
2024-04-30 11:34:02 +0100

    fix: missing datetime_utc

--- backend/bracket/database.py
+++ backend/bracket/database.py
@@ -2,21 +2,21 @@ from typing import Any
 
 import sqlalchemy
 from databases import Database
-from heliclockter import datetime_utc
+from heliclockter import datetime_tz
 
 from bracket.config import config
 
 
-def datetime_decoder(value: str) -> datetime_utc:
+def datetime_decoder(value: str) -> datetime_tz:
     value = value.split(".")[0].replace("+00", "+00:00")
-    return datetime_utc.fromisoformat(value)
+    return datetime_tz.fromisoformat(value)
 
 
 async def asyncpg_init(connection: Any) -> None:
     for timestamp_type in ("timestamp", "timestamptz"):
         await connection.set_type_codec(
             timestamp_type,
-            encoder=datetime_utc.isoformat,
+            encoder=datetime_tz.isoformat,
             decoder=datetime_decoder,
             schema="pg_catalog",
         )
