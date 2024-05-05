8ded5bea2e16bfd47e3b454f0a6bb3b0125c950c
Erik Vroon <erik.vroon@channable.com>
2024-05-05 21:21:17 +0200

    fixup! fixup! fixup! fixup! fixup! Dashboard redesign

--- frontend/src/pages/tournaments/[id]/dashboard/standings.tsx
+++ frontend/src/pages/tournaments/[id]/dashboard/standings.tsx
@@ -37,7 +37,7 @@ export default function Standings() {
         <TournamentHeadTitle tournamentDataFull={tournamentDataFull} />
       </Head>
       <DoubleHeader tournamentData={tournamentDataFull} />
-      <Container mt="1rem">
+      <Container mt="1rem" style={{ overflow: 'scroll' }}>
         <Container style={{ width: '100%', minWidth: '40rem' }} px="0rem">
           <StandingsTable swrTeamsResponse={swrTeamsResponse} />
         </Container>
