cee3fe5fa8dbb421614e0f8e03b8c3e5f9908056
Erik Vroon <erik.vroon22@gmail.com>
2024-05-07 22:18:14 +0200

    Fix empty elo score display (#711)

--- frontend/src/components/info/player_score.tsx
+++ frontend/src/components/info/player_score.tsx
@@ -9,10 +9,11 @@ interface ScoreProps {
 
 export function PlayerScore({ score, min_score, max_score, decimals }: ScoreProps) {
   const percentageScale = 100.0 / (max_score - min_score);
+  const empty = score - min_score === 0;
 
   return (
     <Progress.Root size={20}>
-      <Progress.Section value={percentageScale * (score - min_score)} color="indigo">
+      <Progress.Section value={empty ? 50 : percentageScale * (score - min_score)} color="indigo">
         <Progress.Label>{Number(score).toFixed(decimals)}</Progress.Label>
       </Progress.Section>
     </Progress.Root>
