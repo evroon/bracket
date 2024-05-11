064d427d222fc55afab2bbe8a4b96b401f24f277
Erik Vroon <erik.vroon@channable.com>
2024-05-11 20:46:19 +0200

    fixup! Responsiveness fixes

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -8,6 +8,7 @@ import TournamentModal from '../components/modals/tournament_modal';
 import { capitalize } from '../components/utils/util';
 import { checkForAuthError, getTournaments } from '../services/adapter';
 import Layout from './_layout';
+import classes from './index.module.css';
 
 export default function HomePage() {
   const swrTournamentsResponse = getTournaments();
@@ -20,7 +21,7 @@ export default function HomePage() {
         <Grid.Col span="auto">
           <Title>{capitalize(t('tournaments_title'))}</Title>
         </Grid.Col>
-        <Grid.Col span="content">
+        <Grid.Col span="content" className={classes.fullWithMobile}>
           <TournamentModal swrTournamentsResponse={swrTournamentsResponse} />
         </Grid.Col>
       </Grid>
