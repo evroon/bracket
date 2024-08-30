54b70a7821c4d61dc6666fc4a5e6c107d447a314
Erik Vroon <erik.vroon22@gmail.com>
2024-08-30 19:40:31 +0200

    Fix docker entrypoint (#878)
    With this change, we search for relevant environment variables
    and replace the placeholder value with the actual runtime
    environment variable's value.
    
    We don't have to specify the keys of the environment variables
    anymore and the variables can be unset.
    
    Based on https://www.tomoliver.net/posts/nextjs-docker-public-env-vars

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
-set -evo pipefail
+set -eo pipefail
 
-# Script to replace `NEXT_PUBLIC_*` environment variables because they're set at build-time but we want to set them at runtime in `docker-compose.yml`
-# Define an array of environment variable names
-env_vars=("NEXT_PUBLIC_API_BASE_URL" "NEXT_PUBLIC_HCAPTCHA_SITE_KEY")
+# Script to replace `NEXT_PUBLIC_*` environment variables because they're set
+# at build-time but we want to set them at runtime in `docker-compose.yml`
 
-echo "Checking that we have variables..."
-# Iterate over the array and perform checks
-for var in "${env_vars[@]}"; do
-  test -n "${!var}"
-done
+# The first part wrapped in a function
+makeSedCommands() {
+  printenv | \
+      grep  '^NEXT_PUBLIC' | \
+      sed -r "s/=/ /g" | \
+      xargs -n 2 bash -c 'echo "sed -i \"s#APP_PLACEHOLDER_$0#$1#g\""'
+}
 
-# Iterate over the array and perform substitutions
-for var in "${env_vars[@]}"; do
-  find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_PLACEHOLDER_$var#${!var}#g"
+# Set the delimiter to newlines (needed for looping over the function output)
+IFS=$'\n'
+# For each sed command
+for c in $(makeSedCommands); do
+  # For each file in the .next directory
+  for f in $(find .next -type f); do
+    # Execute the command against the file
+    COMMAND="$c $f"
+    eval $COMMAND
+  done
 done
 
 echo "Starting Nextjs"
