4a81262280a0fb2397a8fed66df783d44fa795d9
Erik Vroon <erik.vroon22@gmail.com>
2024-09-05 18:46:43 +0200

    Fix docker entrypoint (#886)

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 set -eo pipefail
 
-# Script to replace `NEXT_PUBLIC_*` environment variables because they're set
-# at build-time but we want to set them at runtime in `docker-compose.yml`
+if [ -z ${NEXT_PUBLIC_API_BASE_URL+x} ];
+  then echo "Environment variable `NEXT_PUBLIC_API_BASE_URL` is not set, please set it in docker-compose.yml";
+  exit 1;
+fi
 
-# The first part wrapped in a function
-makeSedCommands() {
-  printenv | \
-      grep  '^NEXT_PUBLIC' | \
-      sed -r "s/=/ /g" | \
-      xargs -n 2 bash -c 'echo "sed -i \"s#APP_PLACEHOLDER_$0#$1#g\""'
+
+# Replace the statically built placeholder literals from Dockerfile with run-time
+# the value of the `NEXT_PUBLIC_WEBAPP_URL` environment variable
+replace_placeholder() {
+  find .next public -type f |
+  while read file; do
+      sed -i "s|$1|$2|g" "$file" || true
+  done
 }
 
-# Set the delimiter to newlines (needed for looping over the function output)
-IFS=$'\n'
-# For each sed command
-for c in $(makeSedCommands); do
-  # For each file in the .next directory
-  for f in $(find .next -type f); do
-    # Execute the command against the file
-    COMMAND="$c $f"
-    eval $COMMAND
-  done
-done
+replace_placeholder "http://NEXT_PUBLIC_API_BASE_URL_PLACEHOLDER" "$NEXT_PUBLIC_API_BASE_URL"
+replace_placeholder "NEXT_PUBLIC_HCAPTCHA_SITE_KEY_PLACEHOLDER" "$NEXT_PUBLIC_HCAPTCHA_SITE_KEY"
 
 echo "Starting Nextjs"
 exec "$@"
