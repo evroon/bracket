80efee35720fec4079b76bafb6e4ec7b21d6c5b4
Erik Vroon <erik.vroon@channable.com>
2024-09-05 18:07:10 +0200

    Fix docker entrypoint

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 set -eo pipefail
 
-# Script to replace `NEXT_PUBLIC_*` environment variables because they're set
-# at build-time but we want to set them at runtime in `docker-compose.yml`
+# Replace the statically built http://NEXT_PUBLIC_API_BASE_URL_PLACEHOLDER literal with run-time
+# NEXT_PUBLIC_WEBAPP_URL environment variable
+FROM="http://NEXT_PUBLIC_API_BASE_URL_PLACEHOLDER"
+TO="$NEXT_PUBLIC_API_BASE_URL"
 
-# The first part wrapped in a function
-makeSedCommands() {
-  printenv | \
-      grep  '^NEXT_PUBLIC' | \
-      sed -r "s/=/ /g" | \
-      xargs -n 2 bash -c 'echo "sed -i \"s#APP_PLACEHOLDER_$0#$1#g\""'
-}
-
-# Set the delimiter to newlines (needed for looping over the function output)
-IFS=$'\n'
-# For each sed command
-for c in $(makeSedCommands); do
-  # For each file in the .next directory
-  for f in $(find .next -type f); do
-    # Execute the command against the file
-    COMMAND="$c $f"
-    eval $COMMAND
-  done
+find .next public -type f |
+while read file; do
+    sed -i "s|$FROM|$TO|g" "$file" || true
 done
 
 echo "Starting Nextjs"
