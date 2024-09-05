ac06a0cbc286bee398a11259cc58fe422ccbd068
Erik Vroon <erik.vroon@channable.com>
2024-09-05 18:13:44 +0200

    fixup! Fix docker entrypoint

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -eo pipefail
 
-# Replace the statically built http://NEXT_PUBLIC_API_BASE_URL_PLACEHOLDER literal with run-time
-# NEXT_PUBLIC_WEBAPP_URL environment variable
-FROM="http://NEXT_PUBLIC_API_BASE_URL_PLACEHOLDER"
-TO="$NEXT_PUBLIC_API_BASE_URL"
+# Replace the statically built placeholder literals from Dockerfile with run-time
+# the value of the `NEXT_PUBLIC_WEBAPP_URL` environment variable
+replace_placeholder() {
+  find .next public -type f |
+  while read file; do
+      sed -i "s|$FROM|$TO|g" "$file" || true
+  done
+}
 
-find .next public -type f |
-while read file; do
-    sed -i "s|$FROM|$TO|g" "$file" || true
-done
+replace_placeholder "http://NEXT_PUBLIC_API_BASE_URL_PLACEHOLDER" "$NEXT_PUBLIC_API_BASE_URL"
+replace_placeholder "NEXT_PUBLIC_HCAPTCHA_SITE_KEY_PLACEHOLDER" "$NEXT_PUBLIC_HCAPTCHA_SITE_KEY"
 
 echo "Starting Nextjs"
 exec "$@"
