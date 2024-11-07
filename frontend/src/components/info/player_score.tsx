03a39e273b704e5191e762f255c91941a5558864
Erik Vroon <erik.vroon@channable.com>
2024-11-07 18:35:09 +0100

    fixup! fixup! Fix bugs in presentation dashboard

--- frontend/src/components/info/player_score.tsx
+++ frontend/src/components/info/player_score.tsx
@@ -8,14 +8,22 @@ interface ScoreProps {
   fontSizeInPixels: number;
 }
 
-export function PlayerScore({ score, min_score, max_score, decimals, fontSizeInPixels }: ScoreProps) {
+export function PlayerScore({
+  score,
+  min_score,
+  max_score,
+  decimals,
+  fontSizeInPixels,
+}: ScoreProps) {
   const percentageScale = 100.0 / (max_score - min_score);
   const empty = max_score - min_score === 0;
 
   return (
     <Progress.Root size={fontSizeInPixels * 1.5}>
       <Progress.Section value={empty ? 50 : percentageScale * (score - min_score)} color="indigo">
-        <Progress.Label style={{ fontSize: fontSizeInPixels }}>{Number(score).toFixed(decimals)}</Progress.Label>
+        <Progress.Label style={{ fontSize: fontSizeInPixels }}>
+          {Number(score).toFixed(decimals)}
+        </Progress.Label>
       </Progress.Section>
     </Progress.Root>
   );
