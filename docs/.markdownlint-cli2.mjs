7dbdfc764a2d3f3fd0ea77aa70a003591d0d1741
Erik Vroon <erik.vroon@channable.com>
2025-02-03 09:08:12 +0100

    fixup! Bump markdownlint-rule-relative-links from 3.0.0 to 4.0.1 in /docs

---
+++ docs/.markdownlint-cli2.mjs
@@ -0,0 +1,25 @@
+import relativeLinksRule from "markdownlint-rule-relative-links"
+
+const config = {
+    config: {
+        default: true,
+        "line-length": {
+          strict: true,
+          code_blocks: false,
+          line_length: 100
+        },
+
+        // part of the markdownlint-rule-relative-links plugin
+        "relative-links": true
+    },
+    globs: [
+        "**/*.{md,mdx}"
+    ],
+    ignores: [
+        "node_modules",
+        "static"
+    ],
+  customRules: [relativeLinksRule],
+}
+
+export default config
