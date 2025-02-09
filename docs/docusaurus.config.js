98999ea02117983468b771eb73b068b1fc9e5c54
Erik Vroon <erik.vroon22@gmail.com>
2025-02-09 23:14:55 +0100

    Enable trailingSlash in docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -15,6 +15,7 @@ const config = {
   projectName: "bracket",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
+  trailingSlash: true,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
