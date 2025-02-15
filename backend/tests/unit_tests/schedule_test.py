24bb16400fad7de94925b6b67a3c6c4ee0c0ecbc
Erik Vroon <erik.vroon22@gmail.com>
2025-02-15 15:17:21 +0100

    Fix number of elimination teams (#1123)

--- backend/tests/unit_tests/schedule_test.py
+++ backend/tests/unit_tests/schedule_test.py
@@ -1,3 +1,8 @@
+import re
+
+import pytest
+from fastapi import HTTPException
+
 from bracket.logic.scheduling.elimination import get_number_of_rounds_to_create_single_elimination
 from bracket.logic.scheduling.round_robin import get_number_of_rounds_to_create_round_robin
 
@@ -14,3 +19,12 @@ def test_number_of_rounds_single_elimination() -> None:
     assert get_number_of_rounds_to_create_single_elimination(2) == 1
     assert get_number_of_rounds_to_create_single_elimination(4) == 2
     assert get_number_of_rounds_to_create_single_elimination(8) == 3
+    assert get_number_of_rounds_to_create_single_elimination(16) == 4
+    assert get_number_of_rounds_to_create_single_elimination(32) == 5
+
+    err_msg = re.escape("400: Number of teams invalid, should be one of [2, 4, 8, 16, 32]")
+    with pytest.raises(HTTPException, match=err_msg):
+        get_number_of_rounds_to_create_single_elimination(64)
+
+    with pytest.raises(HTTPException, match=err_msg):
+        get_number_of_rounds_to_create_single_elimination(1)
