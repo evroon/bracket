8a7a1a5c8e78e4474f1a378feec8b062bf74a5e1
Erik Vroon <erik.vroon@channable.com>
2025-02-25 14:52:24 +0100

    fixup! fixup! fixup! Implement sso

--- backend/bracket/logic/sso.py
+++ backend/bracket/logic/sso.py
@@ -1,13 +1,14 @@
-from functools import cache
-from typing import Any
+from typing import Any, assert_never
 
 import aiohttp
-from fastapi_sso import GithubSSO, GoogleSSO, OpenID, SSOBase, create_provider
+from fastapi_sso import GoogleSSO, OpenID, SSOBase, create_provider
 from fastapi_sso.sso.base import DiscoveryDocument
 from httpx import AsyncClient
 
 from bracket.config import config
 from bracket.models.sso import SSOID, SSOConfig, SSOProvider
+
+providers_cache: dict[SSOID, SSOBase] | None = None
 
 
 async def get_discovery_document(discovery_url: str) -> DiscoveryDocument:
@@ -40,13 +41,17 @@ async def get_openid_provider(sso_config: SSOConfig) -> type[SSOBase]:
 
 
 async def build_sso(sso_config: SSOConfig) -> SSOBase:
-    provider_class_lookup: dict[SSOProvider, type[SSOBase]] = {
-        SSOProvider.google: GoogleSSO,
-        SSOProvider.github: GithubSSO,
-        SSOProvider.openid: await get_openid_provider(sso_config),
-    }
+    match sso_config.provider:
+        case SSOProvider.google:
+            provider: type[SSOBase] = GoogleSSO
+        case SSOProvider.github:
+            provider = GoogleSSO
+        case SSOProvider.openid:
+            provider = await get_openid_provider(sso_config)
+        case _ as fallback:
+            assert_never(fallback)
 
-    return provider_class_lookup[sso_config.provider](
+    return provider(
         client_id=sso_config.client_id,
         client_secret=sso_config.client_secret,
         redirect_uri=sso_config.redirect_uri,
@@ -54,8 +59,12 @@ async def build_sso(sso_config: SSOConfig) -> SSOBase:
     )
 
 
-@cache
 async def get_sso_providers() -> dict[SSOID, SSOBase]:
+    global providers_cache  # noqa: PLW0603
+
+    if providers_cache is not None:
+        return providers_cache
+
     configs = []
     if (
         config.sso_1_provider is not None
@@ -68,9 +77,11 @@ async def get_sso_providers() -> dict[SSOID, SSOBase]:
                 provider=config.sso_1_provider,
                 client_id=config.sso_1_client_id,
                 client_secret=config.sso_1_client_secret,
-                redirect_uri=f"{config.base_url}/sso-callback",
+                redirect_uri=f"{config.base_url}/sso-callback/1",
                 allow_insecure_http=config.sso_1_allow_insecure_http_sso,
             )
         )
 
-    return {sso_config.id: await build_sso(sso_config) for sso_config in configs}
+    providers = {sso_config.id: await build_sso(sso_config) for sso_config in configs}
+    providers_cache = providers
+    return providers_cache
