4f89c795a3752304ad22dbfaa03abc0efccc75aa
evroon <erik.vroon22@gmail.com>
2025-04-01 07:00:10 +0000

    Deploying to gh-pages from @ evroon/bracket@a4ee552c94b48612c20ce7a8f6f3cf93130a3ae5 🚀

---
+++ _next/static/chunks/app/page-71ed905eb6aa913b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{2539:(e,s,n)=>{Promise.resolve().then(n.bind(n,7781)),Promise.resolve().then(n.bind(n,1597)),Promise.resolve().then(n.t.bind(n,6874,23)),Promise.resolve().then(n.t.bind(n,3063,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[992,473,441,684,358],()=>s(2539)),_N_E=e.O()}]);
