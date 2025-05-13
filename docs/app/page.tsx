77d4baaccffb85376d7725b6c0d2f5db5a0c460c
Erik Vroon <erik.vroon@channable.com>
2025-05-13 09:26:40 +0200

    fixup! fixup! fixup! Fix title on docs homepage

--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -8,8 +8,8 @@ import { PreviewImage } from "../components/PreviewImage";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Bracket docs",
-  description: "Bracket (docs) - free and open-source tournament system",
+  title: "Bracket documentation",
+  description: "Bracket (documentation) - free and open-source tournament system",
 };
 export default function Page() {
   return (
