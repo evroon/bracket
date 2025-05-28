29d86aba9abcbb9635719f7dd15d630a23d998c7
Erik Vroon <erik.vroon@channable.com>
2025-05-28 11:55:52 +0200

    Improve head tags

--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -6,23 +6,43 @@ import { Cta } from "../components/Cta";
 import "./page.css";
 import { PreviewImage } from "../components/PreviewImage";
 import { Metadata } from "next";
+import {Head} from "nextra/components";
 
 export const metadata: Metadata = {
-  title: "Bracket",
+  title: "Bracket | Open-source tournament system",
   description:
-    "Bracket (documentation) - free and open-source tournament system",
+    "Bracket is a free and open source tournament system. Set up a tournament, add teams, schedule matches, track scores and present live rankings.",
+  openGraph: {
+    title: "Bracket | Open-source tournament system",
+    description: "Bracket is a free and open source tournament system. Set up a tournament, add teams, schedule matches, track scores and present live rankings.",
+    locale: 'en_US',
+    url: "https://docs.bracketapp.nl",
+    siteName: "Bracket",
+    images: [{ url: "https://docs.bracketapp.nl/bracket-social-image.png" }],
+  },
 };
 export default function Page() {
   return (
     <>
-      <Hero />
-      <PreviewImage />
-      <About />
-      <HowItWorks />
-      <Features />
-      {/*<AdvancedFeatures />*/}
-      <Cta />
-      {/*<FAQ />*/}
+        <Head>
+            <script type="application/ld+json">
+            {'{' +
+                '"@context": "https://schema.org",' +
+                '"@type": "WebSite",' +
+                '"name": "Bracket",' +
+                '"alternativeName": "Bracket | Open-source tournament system",' +
+                '"url": "https://docs.bracketapp.nl"' +
+            '}'}
+            </script>
+        </Head>
+        <Hero/>
+        <PreviewImage/>
+        <About/>
+        <HowItWorks/>
+        <Features/>
+        {/*<AdvancedFeatures />*/}
+        <Cta/>
+        {/*<FAQ />*/}
     </>
   );
 }
