0b22db377696f51f83be5e9b437d2618c54bf29e
Erik Vroon <erik.vroon@channable.com>
2025-12-21 17:12:48 +0100

    fixup! Use hey api tournament types

--- backend/openapi/openapi.py
+++ backend/openapi/openapi.py
@@ -7,6 +7,14 @@ def field_is_required(
     field: core_schema.ModelField | core_schema.DataclassField | core_schema.TypedDictField,
     total: bool,
 ) -> bool:
+    """
+    Override the default Pydantic behavior such that we mark all fields as required.
+    Otherwise, Pydantic will mark a field as nullable and optional at the same time
+    which causes hey-api to type fields as `field?: string | null` which is hard to work with.
+
+    Field can still be typed as `type: ["string", "null"]` to allow for nullable values.
+
+    """
     return True
 
 
