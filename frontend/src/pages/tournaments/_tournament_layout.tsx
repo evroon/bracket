a6a7f4570cb5b7df55564a3079d138aa2205d2a9
Erik Vroon <erik.vroon@channable.com>
2025-12-21 18:21:02 +0100

    fixup! Use last hey api response types

--- frontend/src/pages/tournaments/_tournament_layout.tsx
+++ frontend/src/pages/tournaments/_tournament_layout.tsx
@@ -4,8 +4,8 @@ import { HiArchiveBoxArrowDown } from 'react-icons/hi2';
 
 import { TournamentLinks } from '@components/navbar/_main_links';
 import { responseIsValid } from '@components/utils/util';
+import Layout from '@pages/_layout';
 import { checkForAuthError, getTournamentById } from '@services/adapter';
-import Layout from '../_layout';
 
 export default function TournamentLayout({ children, tournament_id }: any) {
   const { t } = useTranslation();
