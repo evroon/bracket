9ffbd623c5bc1ff05578c239be0e121a05d56432
Erik Vroon <11857441+evroon@users.noreply.github.com>
2025-12-30 00:29:34 +0100

    Remove axios plugin from openapi-ts config

--- frontend/openapi-ts.config.js
+++ frontend/openapi-ts.config.js
@@ -3,5 +3,4 @@ import {defineConfig} from '@hey-api/openapi-ts';
 export default defineConfig({
   input: '../backend/openapi/openapi.json', 
   output: 'src/openapi',
-  plugins: ['@hey-api/client-axios'], 
 });
